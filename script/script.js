39be1109910efd110a2b65d0f601c08261d79f5c
Gustavo F. Silva <108156436+gustavofdasilva@users.noreply.github.com>
2022-09-12 21:52:05 -0300

    continuando script

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 var resultBox = document.querySelector("div#result>input");
+var intResult;
 
 function clearResult() {
     resultBox.value = "";
@@ -6,4 +7,16 @@ function clearResult() {
 
 function add(newValue) {
     resultBox.value += newValue;
+}
+
+function result() {
+    var resultString = String(resultBox.value);
+    var resultNumber = Number(resultBox.value);
+    
+    for(var i = 0; i < String(resultBox.value).length ; i++) {
+        
+        if(Number(resultString.substring(i,i+1)) != isNaN) {
+            window.alert(`Índice ${i} e Número ${Number(resultString.substring(i,i+1))}`);
+        } 
+    }
 }
