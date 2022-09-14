b1c795b789ed645c347984975fb77b3483db83ea
Gustavo F. Silva <108156436+gustavofdasilva@users.noreply.github.com>
2022-09-14 20:50:59 -0300

    finalizando projeto!
    Por enquanto, sem mudanças esperadas, caso tudo esteja funcionando da maneira que deveria, o projeto está finalizado!

--- script/script.js
+++ script/script.js
@@ -1,22 +1,23 @@
 var resultBox = document.querySelector("div#result>input");
-var intResult;
 
 function clearResult() {
     resultBox.value = "";
 }
 
 function add(newValue) {
-    resultBox.value += newValue;
+    if(!isNaN(newValue)) {
+        resultBox.value += newValue;
+    } else {
+        if(!isNaN(String(resultBox.value).slice(-1))) {
+            resultBox.value += newValue;
+        }
+    }
 }
 
 function result() {
     var resultString = String(resultBox.value);
-    var resultNumber = Number(resultBox.value);
-    
-    for(var i = 0; i < String(resultBox.value).length ; i++) {
-        
-        if(Number(resultString.substring(i,i+1)) != isNaN) {
-            window.alert(`Índice ${i} e Número ${Number(resultString.substring(i,i+1))}`);
-        } 
-    }
+    var resultFinal = eval(resultString);
+
+
+    resultBox.value = resultFinal;
 }
